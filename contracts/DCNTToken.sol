13fa7583fc73d1fe48549afc48526e8cf32a12b2
Kellar <kellar.base09@gmail.com>
2022-02-25 14:32:02 +0000

    Merge airdrop functionality with token

--- contracts/DCNTToken.sol
+++ contracts/DCNTToken.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.11;
 
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
@@ -10,14 +11,45 @@ contract DCNTToken is ERC20Votes, Ownable {
     uint32 public constant MINIMUM_MINT_INTERVAL = 365 days;
     uint8 public constant MINT_CAP_BPS = 200; // 2%
 
+    bytes32 public immutable merkleRoot;
+    address public immutable returnAddress;
+    uint64 public immutable endDate;
+
+    mapping(address => bool) public claimed;
+
+    event AirdropClaimed(address claimant, uint256 amount);
+    event AirdropEnded();
+
+    error AlreadyClaimed();
+    error NotEligible();
+    error AirdropStillActive();
+
     error MintExceedsMaximum();
     error MintTooSoon();
 
     /// @param freeSupply amount of tokens to mint at Token Generation Event
-    constructor(uint256 freeSupply)
-        ERC20("Decent", "DCNT")
-        ERC20Permit("Decent")
-    {
+    /// @param airdropSupply amount of tokens to allocated for airdrop
+    /// @param _merkleRoot The root of a merkle tree constructed from
+    ///  a dataset containing eligible addresses and their claims.
+    ///   See
+    ///       - https://github.com/miguelmota/merkletreejs for generating tree in JS
+    ///       - https://github.com/miguelmota/merkletreejs-solidity#example for example usage
+    /// @param _endDate timestamp before which claimants may continue to claim airdrop
+    /// @param _returnAddress unclaimed tokens will be sent to this address after endAirdrop is called
+    ///
+    /// @dev The 'leaves' param in (https://github.com/miguelmota/merkletreejs) should be a list of
+    ///  keccak256 hashes of "abi.encodePacked(claimantAddress, claim)"s for this contract's verification
+    ///  to work. Pair sorting should be enabled.
+    constructor(
+        uint256 freeSupply,
+        uint256 airdropSupply,
+        bytes32 _merkleRoot,
+        uint64 _endDate,
+        address _returnAddress
+    ) ERC20("Decent", "DCNT") ERC20Permit("Decent") {
+        merkleRoot = _merkleRoot;
+        returnAddress = _returnAddress;
+        endDate = _endDate;
         _mint(msg.sender, freeSupply);
         nextMint = uint128(block.timestamp + MINIMUM_MINT_INTERVAL);
     }
@@ -38,5 +70,54 @@ contract DCNTToken is ERC20Votes, Ownable {
 
         nextMint = uint128(block.timestamp + MINIMUM_MINT_INTERVAL);
         _mint(dest, amount);
+    }
+
+    /// @notice Claim an airdrop if eligigle
+    /// @param _amount to claim; must necessarily be equal to the amount allocated to claimant
+    /// @param _proof Merkle Proof to validate claim
+    /// @param _delegatee The address to delegate the claimant's voting power to
+    function claim(
+        uint256 _amount,
+        address _delegatee,
+        bytes32[] memory _proof
+    ) public {
+        if (claimed[msg.sender] == true) {
+            revert AlreadyClaimed();
+        }
+        if (!verify(msg.sender, _amount, _proof)) {
+            revert NotEligible();
+        }
+
+        claimed[msg.sender] = true;
+        emit AirdropClaimed(msg.sender, _amount);
+
+        _delegate(msg.sender, _delegatee);
+        transfer(msg.sender, _amount);
+    }
+
+    /// @notice Transfer unclaimed tokens to pre-designated address. Emits AirdropEnded()
+    function endAirdrop() public onlyOwner {
+        if (block.timestamp < endDate) {
+            revert AirdropStillActive();
+        }
+
+        transfer(returnAddress, balanceOf(address(this)));
+        emit AirdropEnded();
+    }
+    /// @notice Verify an airdrop claim. Does not transfer tokens.
+    /// @param _claimant the account making the claim, to which airdrop will be sent
+    /// @param _amount to claim; must necessarily be equal to the amount allocated to _claimant
+    /// @param _proof Merkle Proof to validate claim
+    function verify(
+        address _claimant,
+        uint256 _amount,
+        bytes32[] memory _proof
+    ) public view returns (bool) {
+        return
+            MerkleProof.verify(
+                _proof,
+                merkleRoot,
+                keccak256(abi.encodePacked(_claimant, _amount))
+            );
     }
 }
--- test/DCNTToken.test.ts
+++ test/DCNTToken.test.ts
@@ -2,129 +2,319 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
-import { DCNTToken, DCNTToken__factory } from "../typechain";
+import { DCNTToken } from "../typechain";
+import { MerkleTree } from "merkletreejs";
+
 import time from "./time";
+import { makeLeaf, makeLeaves, constructMerkleTree } from "../scripts/airdrop_helpers";
 
 describe("DCNTToken", function () {
-  let owner: SignerWithAddress, nonOwner: SignerWithAddress;
+  let owner: SignerWithAddress,
+    nonOwner: SignerWithAddress,
+    recoveryDest: SignerWithAddress,
+    claimant1: SignerWithAddress,
+    claimantN: SignerWithAddress,
+    claimant2: SignerWithAddress;
+
   let dcnt: DCNTToken;
   let freeMintWhole: number;
+  const airdropSupply: BigNumber = BigNumber.from(10_000);
   let freeMintTotal: BigNumber;
 
+  let tree: MerkleTree;
+  let leaves: string[];
+  let airdropClaimants: {
+    addr: string;
+    claim: BigNumber;
+  }[];
+
+  let airdropEndDate: number;
+
+
+
   beforeEach(async function () {
-    [owner, nonOwner] = await ethers.getSigners();
+    [owner, nonOwner, recoveryDest, claimant1, claimant2, claimantN] = await ethers.getSigners();
+
+    airdropClaimants = [
+      { addr: claimant1.address, claim: BigNumber.from(1000) },
+      { addr: claimant2.address, claim: BigNumber.from(9000) },
+    ];
+
+    // Prepare merkle tree of claimants
+    leaves = makeLeaves(airdropClaimants);
+    tree = constructMerkleTree(leaves);
+
+    airdropEndDate = await time.latest() + time.duration.years(1);
 
     freeMintWhole = 1_000_000_000;
     freeMintTotal = ethers.utils.parseEther(freeMintWhole.toString());
 
-    dcnt = await new DCNTToken__factory(owner).deploy(freeMintTotal);
+    // Deploy token contract
+    let _DCNTToken = await ethers.getContractFactory("DCNTToken");
+    dcnt = await _DCNTToken.deploy(
+      freeMintTotal,
+      airdropSupply,
+      tree.getHexRoot(),
+      airdropEndDate,
+      recoveryDest.address,
+    );
     await dcnt.deployed();
   });
 
-  describe("Minting the correct amount of initial free tokens", function () {
-    let totalSupply: BigNumber;
+  describe("Token features", function () {
+    describe("Minting the correct amount of initial free tokens", function () {
+      let totalSupply: BigNumber;
 
-    beforeEach(async function () {
-      totalSupply = await dcnt.totalSupply();
+      beforeEach(async function () {
+        totalSupply = await dcnt.totalSupply();
+      });
+
+      it("Should mint the correct amount of initial tokens in wei (decimals)", async function () {
+        expect(totalSupply).to.equal(freeMintTotal);
+      });
+
+      it("Should mint the correct amount of initial tokens in whole numbers", async function () {
+        expect(parseInt(ethers.utils.formatEther(totalSupply))).to.eq(
+          freeMintWhole
+        );
+      });
     });
 
-    it("Should mint the correct amount of initial tokens in wei (decimals)", async function () {
-      expect(totalSupply).to.equal(freeMintTotal);
-    });
+    describe("Minting more tokens", function () {
+      let originalTotalSupply: BigNumber,
+        minimumMintInterval: number,
+        mintCapBPs: number,
+        nextMint: BigNumber;
 
-    it("Should mint the correct amount of initial tokens in whole numbers", async function () {
-      expect(parseInt(ethers.utils.formatEther(totalSupply))).to.eq(
-        freeMintWhole
-      );
+      beforeEach(async function () {
+        [originalTotalSupply, minimumMintInterval, mintCapBPs, nextMint] =
+          await Promise.all([
+            dcnt.totalSupply(),
+            dcnt.MINIMUM_MINT_INTERVAL(),
+            dcnt.MINT_CAP_BPS(),
+            dcnt.nextMint(),
+          ]);
+
+        await time.increaseTo(nextMint.toNumber());
+      });
+
+      describe("Depending on the caller address", function () {
+        let oneWei: number;
+
+        beforeEach(function () {
+          oneWei = 1;
+        });
+
+        it("Should allow owner to mint 1 wei", async function () {
+          await dcnt.mint(owner.address, oneWei);
+          expect(await dcnt.totalSupply()).to.eq(originalTotalSupply.add(oneWei));
+        });
+
+        it("Should not allow non-owner to mint 1 wei", async function () {
+          await expect(
+            dcnt.connect(nonOwner).mint(owner.address, oneWei)
+          ).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+      });
+
+      describe("Depending on the amount", function () {
+        let maxToMint: BigNumber;
+
+        beforeEach(async function () {
+          // calculate the max amount of tokens that can be minted
+          maxToMint = originalTotalSupply.mul(mintCapBPs).div(10000);
+        });
+
+        describe("Max mint amount", function () {
+          it("Should allow the owner to mint more tokens", async function () {
+            await dcnt.mint(owner.address, maxToMint);
+            expect(await dcnt.totalSupply()).to.eq(
+              originalTotalSupply.add(maxToMint)
+            );
+          });
+        });
+
+        describe("Over max mint amount", function () {
+          let overMaxToMint: BigNumber;
+
+          beforeEach(function () {
+            overMaxToMint = maxToMint.add(1);
+          });
+
+          it("Should not allow the owner to mint more tokens", async function () {
+            await expect(
+              dcnt.mint(owner.address, overMaxToMint)
+            ).to.be.revertedWith("MintExceedsMaximum()");
+          });
+        });
+      });
+
+      describe("Depending on the time", function () {
+        beforeEach(async function () {
+          // dummy mint to set the timeout interval
+          await dcnt.mint(owner.address, 0);
+        });
+
+        it("Should not allow the owner to mint after having just minted", async function () {
+          await expect(dcnt.mint(owner.address, 1)).to.be.revertedWith(
+            "MintTooSoon()"
+          );
+        });
+
+        it("Should allow the owner to mint after the minimum mint interval", async function () {
+          await time.increase(minimumMintInterval);
+          const toMint = 1;
+          await dcnt.mint(owner.address, toMint);
+          expect(await dcnt.totalSupply()).to.eq(originalTotalSupply.add(toMint));
+        });
+      });
     });
   });
 
-  describe("Minting more tokens", function () {
-    let originalTotalSupply: BigNumber,
-      minimumMintInterval: number,
-      mintCapBPs: number,
-      nextMint: BigNumber;
-
-    beforeEach(async function () {
-      [originalTotalSupply, minimumMintInterval, mintCapBPs, nextMint] =
-        await Promise.all([
-          dcnt.totalSupply(),
-          dcnt.MINIMUM_MINT_INTERVAL(),
-          dcnt.MINT_CAP_BPS(),
-          dcnt.nextMint(),
-        ]);
-
-      await time.increaseTo(nextMint.toNumber());
-    });
-
-    describe("Depending on the caller address", function () {
-      let oneWei: number;
-
-      beforeEach(function () {
-        oneWei = 1;
+  describe("Airdrop features", function () {
+    describe("Initial deployment state", function () {
+      it("Should have totalClaimable DCNT tokens allocated for airdrop", async function () {
+        let airdropBalance = await dcnt.balanceOf(dcnt.address);
+        expect(airdropBalance).to.equal(airdropSupply);
       });
 
-      it("Should allow owner to mint 1 wei", async function () {
-        await dcnt.mint(owner.address, oneWei);
-        expect(await dcnt.totalSupply()).to.eq(originalTotalSupply.add(oneWei));
-      });
+      it("Should have correctly set all initial state vars", async function () {
+        let _endDate = await dcnt.endDate();
+        let _returnAddress = await dcnt.returnAddress();
+        let _root = await dcnt.merkleRoot();
 
-      it("Should not allow non-owner to mint 1 wei", async function () {
-        await expect(
-          dcnt.connect(nonOwner).mint(owner.address, oneWei)
-        ).to.be.revertedWith("Ownable: caller is not the owner");
+        expect(_endDate).to.equal(airdropEndDate);
+        expect(_returnAddress).to.equal(recoveryDest.address);
+        expect(_root).to.equal(tree.getHexRoot());
       });
     });
 
-    describe("Depending on the amount", function () {
-      let maxToMint: BigNumber;
+    describe("Airdrop claims", function () {
+      describe("Given valid proof", function () {
+        it("Should transfer claimant's DCNT claim from airdrop to them, and emit AirdropClaimed", async function () {
+          let _leaf1 = leaves[0];
+          const proof = tree.getHexProof(_leaf1);
 
-      beforeEach(async function () {
-        // calculate the max amount of tokens that can be minted
-        maxToMint = originalTotalSupply.mul(mintCapBPs).div(10000);
-      });
+          let eligibleClaim = dcnt.connect(claimant1).claim(BigNumber.from(airdropClaimants[0].claim), claimant1.address, proof);
 
-      describe("Max mint amount", function () {
-        it("Should allow the owner to mint more tokens", async function () {
-          await dcnt.mint(owner.address, maxToMint);
-          expect(await dcnt.totalSupply()).to.eq(
-            originalTotalSupply.add(maxToMint)
-          );
+          expect(eligibleClaim).to.emit(dcnt, "AirdropClaimed");
+
+          await eligibleClaim;
+
+          let claimant1Balance = await dcnt.balanceOf(claimant1.address);
+          let airdropBalance = await dcnt.balanceOf(dcnt.address);
+
+          expect(claimant1Balance).to.equal(airdropClaimants[0].claim);
+          expect(airdropBalance).to.equal(airdropSupply.sub(airdropClaimants[0].claim));
+        });
+
+        it("Should revert with AlreadyClaimed() if already claimed", async function () {
+          let _leaf1 = leaves[0];
+          const proof = tree.getHexProof(_leaf1);
+
+          let _ = await dcnt.connect(claimant1).claim(BigNumber.from(airdropClaimants[0].claim), claimant1.address, proof);
+          await _.wait();
+
+          let _reclaim = dcnt.connect(claimant1).claim(BigNumber.from(airdropClaimants[0].claim), claimant1.address, proof);
+          expect(_reclaim).to.be.revertedWith('AlreadyClaimed()');
+
+          let airdropBalance = await dcnt.balanceOf(dcnt.address);
+          expect(airdropBalance).to.equal(airdropSupply.sub(airdropClaimants[0].claim));
+        });
+
+        it("Should allow eligible transfers after end date as long as endAirdrop has not been called", async function () {
+          let _leaf1 = leaves[0];
+          const proof = tree.getHexProof(_leaf1);
+
+          await time.increase(time.duration.years(10));
+
+          await dcnt.connect(claimant1).claim(BigNumber.from(airdropClaimants[0].claim), claimant1.address, proof);
+
+          let claimant1Balance = await dcnt.balanceOf(claimant1.address);
+          let airdropBalance = await dcnt.balanceOf(dcnt.address);
+
+          expect(claimant1Balance).to.equal(airdropClaimants[0].claim);
+          expect(airdropBalance).to.equal(airdropSupply.sub(airdropClaimants[0].claim));
+        });
+
+        it("Should revert after endAirdrop has been called", async function () {
+          let _leaf1 = leaves[0];
+          const proof = tree.getHexProof(_leaf1);
+
+          await time.increase(time.duration.years(1));
+          await dcnt.endAirdrop();
+
+          let lateClaim = dcnt.claim(BigNumber.from(airdropClaimants[0].claim), claimant1.address, proof);
+
+          expect(lateClaim).to.be.reverted;
         });
       });
 
-      describe("Over max mint amount", function () {
-        let overMaxToMint: BigNumber;
+      describe("Given invalid proof", function () {
+        it("Should revert with NotEligible() due to mismatching claim", async function () {
+          let _leaf1 = leaves[0];
+          const proof = tree.getHexProof(_leaf1);
 
-        beforeEach(function () {
-          overMaxToMint = maxToMint.add(1);
+          let attemptToClaim = dcnt.connect(claimant1).claim(
+            BigNumber.from(airdropClaimants[0].claim.add(1)), claimant1.address, proof
+          )
+
+          expect(attemptToClaim).to.be.revertedWith('NotEligible()');
+
+          let claimant1Balance = await dcnt.balanceOf(claimant1.address);
+          let airdropBalance = await dcnt.balanceOf(dcnt.address);
+
+          expect(claimant1Balance).to.equal(0);
+          expect(airdropBalance).to.equal(airdropSupply);
         });
 
-        it("Should not allow the owner to mint more tokens", async function () {
-          await expect(
-            dcnt.mint(owner.address, overMaxToMint)
-          ).to.be.revertedWith("MintExceedsMaximum()");
+        it("Should revert with NotEligible() due to address not in airdrop", async function () {
+          let _leaf = makeLeaf(claimantN.address, BigNumber.from(1));
+          const proof = tree.getHexProof(_leaf);
+
+          let attemptToClaim = dcnt.connect(claimant1).claim(BigNumber.from(1), claimant1.address, proof);
+          expect(attemptToClaim).to.be.revertedWith('NotEligible()');
+
+          let claimantBalance = await dcnt.balanceOf(claimantN.address);
+          let airdropBalance = await dcnt.balanceOf(dcnt.address);
+
+          expect(claimantBalance).to.equal(0);
+          expect(airdropBalance).to.equal(airdropSupply);
         });
       });
     });
 
-    describe("Depending on the time", function () {
-      beforeEach(async function () {
-        // dummy mint to set the timeout interval
-        await dcnt.mint(owner.address, 0);
+    describe("End airdrop", function () {
+      describe("When called before end date", function () {
+        it("Should revert with AirdropStillActive()", async function () {
+          expect(dcnt.endAirdrop()).to.be.revertedWith("AirdropStillActive()");
+        });
       });
 
-      it("Should not allow the owner to mint after having just minted", async function () {
-        await expect(dcnt.mint(owner.address, 1)).to.be.revertedWith(
-          "MintTooSoon()"
-        );
-      });
+      describe("When called after end date", function () {
+        it("Should transfer all unclaimed airdrops to recovery address, and emit AirdropEnded", async function () {
+          await time.increase(time.duration.years(1));
+          let endAirdrop = dcnt.endAirdrop();
 
-      it("Should allow the owner to mint after the minimum mint interval", async function () {
-        await time.increase(minimumMintInterval);
-        const toMint = 1;
-        await dcnt.mint(owner.address, toMint);
-        expect(await dcnt.totalSupply()).to.eq(originalTotalSupply.add(toMint));
+          expect(endAirdrop).to.emit(dcnt, "AirdropEnded");
+
+          await endAirdrop;
+
+          let revoveryDestBalance = await dcnt.balanceOf(recoveryDest.address);
+          expect(revoveryDestBalance).to.equal(airdropSupply);
+
+          let airdropBalance = await dcnt.balanceOf(dcnt.address);
+          expect(airdropBalance).to.equal(0);
+        });
+
+        it("Should not allow non owner to end airdrop", async function () {
+          await time.increase(time.duration.years(1));
+          expect(dcnt.connect(claimant1).endAirdrop()).to.be.reverted;
+        });
+
+        it("Should not allow non owner to end airdrop", async function () {
+          expect(dcnt.connect(claimant1).endAirdrop()).to.be.reverted;
+        });
       });
     });
   });
